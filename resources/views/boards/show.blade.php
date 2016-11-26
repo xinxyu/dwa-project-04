696b7a6c4955c472b3e0cc15db70b93b27fdcc78
Xin Yu <xin.yu55@gmail.com>
2016-11-25 23:44:53 -0500

    styling changes on delete board confirmation message

--- resources/views/boards/show.blade.php
+++ resources/views/boards/show.blade.php
@@ -55,8 +55,8 @@
                 <div class="modal-dialog">
 
                     <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
+                    <div class="modal-content text-center">
+                        <div class="modal-header ">
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                             <h4 class="modal-title">Delete Board</h4>
                         </div>
