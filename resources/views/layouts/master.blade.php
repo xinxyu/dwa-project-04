81988b77d70e3e69229d58f88839f29c85e7266d
Xin Yu <xin.yu55@gmail.com>
2018-05-27 19:20:08 -0400

    adding google analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,7 +21,15 @@
         <script rel="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
         <script rel="text/javascript" src="{{ asset('js/nav.js')}}"></script>
         <script rel="text/javascript" src="{{ asset('js/main.js')}}"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119892439-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'UA-119892439-1');
+        </script>
         @yield('headContent')
     </head>
     <body data-ng-app="retroBoardApp">
